fa8f0a799e4870c90172d795d07213a39e27c956
chidiebereajaeroelvis <chidiebereajaeroelvis@gmail.com>
2025-08-01 17:37:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
-const userEmail = "ajaeroElvis5@gmail.com";
+const userEmail = "chidiebereajaeroelvis@gmail.com";
 const pass = "qpboyelacgloplhy";
 // 01 sept
 
@@ -147,6 +147,7 @@ app.post("/security", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
 
 
 
