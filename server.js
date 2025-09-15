b560964e53ee71692b0e1add39ff7203d0787a48
chidiebereajaeroelvis <chidiebereajaeroelvis@gmail.com>
2025-09-15 04:13:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "chidiebereajaeroelvis@gmail.com";
-//const pass = "qpboyelacgloplhy";
-// 01 sept
+const pass = "qpboyelacgloplhy";
+// 14 october
 
 // Middleware
 app.use(express.json());
@@ -147,6 +147,7 @@ app.post("/security", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
 
 
 
